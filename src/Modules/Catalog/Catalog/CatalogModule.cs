1e302160f703f2a160a28e40d1d63a40cad9f8b0
Aydin BASKARA <aydinbskr@gmail.com>
2024-09-29 15:21:40 +0300

    CatalogDbContext added.

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -8,6 +8,8 @@ namespace Catalog
     {
         public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
         {
+            var connectionString = configuration.GetConnectionString("Database");
+            services.AddDbContext<CatalogDbContext>(options => options.UseNpgsql(connectionString));
             return services;
         }
 
--- src/Modules/Catalog/Catalog/GlobalUsings.cs
+++ src/Modules/Catalog/Catalog/GlobalUsings.cs
@@ -2,3 +2,7 @@
 global using Shared.DDD;
 global using Catalog.Products.Models;
 global using Catalog.Products.Events;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using System.Reflection;
+global using Catalog.Data;
