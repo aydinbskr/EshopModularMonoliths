0ba0dc98a1f98783a6466325b6d797cff6b471f3
Aydin BASKARA <aydinbskr@gmail.com>
2025-04-26 16:57:35 +0300

    WEB UI added.

---
+++ src/Bootstrapper/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,13 @@
+﻿namespace Shopping.Web.Services;
+
+public interface ICatalogService
+{
+    [Get("/products?pageNumber={pageNumber}&pageSize={pageSize}")]
+    Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+    
+    [Get("/products/{id}")]
+    Task<GetProductByIdResponse> GetProduct(Guid id);
+    
+    [Get("/products/category/{category}")]
+    Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
+}
